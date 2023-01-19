ed79db69d22c922b953ac0023215c8ae248429f3
Iamrahuljaiswal <63348065+Iamrahuljaiswal@users.noreply.github.com>
2023-01-19 17:20:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,121 @@
+function india() {
+    let zone = { timeZone: 'Asia/Kolkata' };
+    let currentDate = new Date().toLocaleDateString('en-US', zone);
+    let currentTime = new Date().toLocaleTimeString('en-US', zone);
+    document.querySelector('#indian-date').innerText = currentDate
+    document.querySelector('#indian-time').innerText = currentTime
+
+}
+setInterval(india, 1000)
+
+function france() {
+    let zone = { timeZone: 'Europe/Paris' };
+    let currentDate = new Date().toLocaleDateString('en-US', zone);
+    let currentTime = new Date().toLocaleTimeString('en-US', zone);
+    document.querySelector('#france-date').innerText = currentDate
+    document.querySelector('#france-time').innerText = currentTime
+
+}
+setInterval(france, 1000)
+
+function russia() {
+    let zone = { timeZone: 'Europe/Moscow' };
+    let currentDate = new Date().toLocaleDateString('en-US', zone);
+    let currentTime = new Date().toLocaleTimeString('en-US', zone);
+    document.querySelector('#russia-date').innerText = currentDate
+    document.querySelector('#russia-time').innerText = currentTime
+
+}
+setInterval(russia, 1000)
+
+function us() {
+    let zone = { timeZone: 'America/New_york' };
+    let currentDate = new Date().toLocaleDateString('en-US', zone);
+    let currentTime = new Date().toLocaleTimeString('en-US', zone);
+    document.querySelector('#us-date').innerText = currentDate
+    document.querySelector('#us-time').innerText = currentTime
+
+}
+setInterval(us, 1000)
+
+function australia() {
+    let zone = { timeZone: 'Australia/Sydney' };
+    let currentDate = new Date().toLocaleDateString('en-US', zone);
+    let currentTime = new Date().toLocaleTimeString('en-US', zone);
+    document.querySelector('#australia-date').innerText = currentDate
+    document.querySelector('#australia-time').innerText = currentTime
+
+}
+setInterval(australia, 1000)
+
+function canada() {
+    let zone = { timeZone: 'America/Moncton' };
+    let currentDate = new Date().toLocaleDateString('en-US', zone);
+    let currentTime = new Date().toLocaleTimeString('en-US', zone);
+    document.querySelector('#canada-date').innerText = currentDate
+    document.querySelector('#canada-time').innerText = currentTime
+
+}
+setInterval(canada, 1000)
+
+function denmark() {
+    let zone = { timeZone: 'Europe/Copenhagen' };
+    let currentDate = new Date().toLocaleDateString('en-US', zone);
+    let currentTime = new Date().toLocaleTimeString('en-US', zone);
+    document.querySelector('#denmark-date').innerText = currentDate
+    document.querySelector('#denmark-time').innerText = currentTime
+
+}
+setInterval(denmark, 1000)
+
+function nz() {
+    let zone = { timeZone: 'Pacific/Chatham' };
+    let currentDate = new Date().toLocaleDateString('en-US', zone);
+    let currentTime = new Date().toLocaleTimeString('en-US', zone);
+    document.querySelector('#nz-date').innerText = currentDate
+    document.querySelector('#nz-time').innerText = currentTime
+
+}
+setInterval(nz, 1000)
+
+function brazil() {
+    let zone = { timeZone: 'America/Maceio' };
+    let currentDate = new Date().toLocaleDateString('en-US', zone);
+    let currentTime = new Date().toLocaleTimeString('en-US', zone);
+    document.querySelector('#brazil-date').innerText = currentDate
+    document.querySelector('#brazil-time').innerText = currentTime
+
+}
+setInterval(brazil, 1000)
+
+function indonesia() {
+    let zone = { timeZone: 'Asia/Jakarta' };
+    let currentDate = new Date().toLocaleDateString('en-US', zone);
+    let currentTime = new Date().toLocaleTimeString('en-US', zone);
+    document.querySelector('#indonesia-date').innerText = currentDate
+    document.querySelector('#indonesia-time').innerText = currentTime
+
+}
+setInterval(indonesia, 1000)
+
+
+
+function italy() {
+    let zone = { timeZone: 'Europe/Rome' };
+    let currentDate = new Date().toLocaleDateString('en-US', zone);
+    let currentTime = new Date().toLocaleTimeString('en-US', zone);
+    document.querySelector('#italy-date').innerText = currentDate
+    document.querySelector('#italy-time').innerText = currentTime
+
+}
+setInterval(italy, 1000)
+
+function pakistan() {
+    let zone = { timeZone: 'Asia/Karachi' };
+    let currentDate = new Date().toLocaleDateString('en-US', zone);
+    let currentTime = new Date().toLocaleTimeString('en-US', zone);
+    document.querySelector('#pakistan-date').innerText = currentDate
+    document.querySelector('#pakistan-time').innerText = currentTime
+
+}
+setInterval(pakistan, 1000)
